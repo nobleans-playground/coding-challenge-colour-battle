25443c50b46da060f9cd4bd07655ec0860d2bef7
Hein Wessels <heinwessels93@gmail.com>
2023-02-04 09:36:03 +0100

    Run in github pages

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import pygame, sys, random
+import asyncio
+import pygame, sys
 from pygame.locals import *
-pygame.init()
- 
+
 # Colours
 BACKGROUND = (255, 255, 255)
 
 from world import World
  
 # Game Setup
-FPS = 5
+FPS = 10
 fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
@@ -22,13 +22,14 @@ world.add_bot(RamboTheRando())
 world.add_bot(RamboTheRando())
 world.create()
  
+pygame.init()
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 CANVAS = pygame.Surface(WINDOW.get_size(), pygame.SRCALPHA)
 CANVAS.set_alpha(200)
 pygame.display.set_caption('King of the Hill')
  
 # The main function that controls the game
-def main () :
+async def main ():
   looping = True
   
   # The main game loop
@@ -45,10 +46,10 @@ def main () :
     world.step()
     
     # Render elements of the game
-    WINDOW.fill((255, 255, 255))
+    WINDOW.fill(BACKGROUND)
     world.draw(CANVAS, WINDOW) 
     WINDOW.blit(CANVAS, (0,0))
     pygame.display.update()
-    fpsClock.tick(FPS)
+    await asyncio.sleep(1 / FPS)
  
-main()
+asyncio.run(main())
