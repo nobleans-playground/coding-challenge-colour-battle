39ec57419686eeffc69fd857def58eb8d96bb956
Hein Wessels <heinwessels93@gmail.com>
2023-03-02 13:39:49 +0100

    Submission: Picasso by Daniel

--- robots/bot_list.py
+++ robots/bot_list.py
@@ -3,12 +3,12 @@ import importlib
 from robots.short_sighted_steve import ShortSightedSteve
 
 RamboTheRando = importlib.import_module("robots.coding-challenge-bot-template.rambo_the_rando").RamboTheRando
+DanielsRobot = importlib.import_module("robots.DanielsRobot.Daniels_Bot").Daniels_Bot
 
 # Add new bots to the bottom of this list
 # to maintain the same colours as much as possible
 BotList = [
     RamboTheRando,
     ShortSightedSteve,
-    RamboTheRando,
-    ShortSightedSteve,
+    DanielsRobot
 ]
