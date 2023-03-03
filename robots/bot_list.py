75f1a72a0fb749fec474bd2b3dc9e00fcac71c5e
Hein Wessels <heinwessels93@gmail.com>
2023-03-03 09:04:19 +0100

    Submission: AtillaTheAttacker by Jorik

--- robots/bot_list.py
+++ robots/bot_list.py
@@ -4,11 +4,13 @@ from robots.short_sighted_steve import ShortSightedSteve
 
 RamboTheRando = importlib.import_module("robots.coding-challenge-bot-template.rambo_the_rando").RamboTheRando
 DanielsRobot = importlib.import_module("robots.DanielsRobot.Daniels_Bot").Daniels_Bot
+JorikAtilla = importlib.import_module("robots.jorik.atilla").AtillaTheAttacker
 
 # Add new bots to the bottom of this list
 # to maintain the same colours as much as possible
 BotList = [
     RamboTheRando,
     ShortSightedSteve,
-    DanielsRobot
+    DanielsRobot,
+    JorikAtilla
 ]
