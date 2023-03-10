1a65b809ad0a194a0c24848b10c114a3f0aa4c35
Hein Wessels <heinwessels93@gmail.com>
2023-03-10 14:54:53 +0100

    Submission: The Clueless African by JP

--- robots/bot_list.py
+++ robots/bot_list.py
@@ -5,6 +5,7 @@ from robots.short_sighted_steve import ShortSightedSteve
 RamboTheRando = importlib.import_module("robots.coding-challenge-bot-template.rambo_the_rando").RamboTheRando
 DanielsRobot = importlib.import_module("robots.DanielsRobot.Daniels_Bot").Daniels_Bot
 JorikAtilla = importlib.import_module("robots.jorik.atilla").AtillaTheAttacker
+TheCluelessAfrican = importlib.import_module("robots.jp.The_no_clue_African").TheCluelessAfrican
 
 # Add new bots to the bottom of this list
 # to maintain the same colours as much as possible
@@ -12,5 +13,6 @@ BotList = [
     RamboTheRando,
     ShortSightedSteve,
     DanielsRobot,
-    JorikAtilla
+    JorikAtilla,
+    TheCluelessAfrican
 ]
